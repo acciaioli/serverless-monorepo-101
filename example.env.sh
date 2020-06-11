a8729f9760f5bf760cad66036d3639a3421a56e1
acciaioli <acci.valverde@protonmail.com>
2020-06-11 21:48:26 +0100

    wip ci

--- example.env.sh
+++ example.env.sh
@@ -1,6 +1,4 @@
-export ENV=dev
-export DEPLOYMENT_BUCKET=serverless-monorepo-101-deployments
-	Service     string `required:"true"`
-	S3Bucket    string `required:"true"`
-	GithubRepo  string `required:"true"`
-	GithubToken string `required:"true"`
+export ENV="dev"
+export DEPLOYMENT_BUCKET="aws-s3-bucket"
+export GITHUB_REPO="owner/repo"
+export GITHUB_TOKEN="****"
