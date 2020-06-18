eefe0edfc7032b4e7f226a1a361bad5496c25ada
acciaioli <acci.valverde@protonmail.com>
2020-06-18 23:47:04 +0100

    env wip

--- example.env.sh
+++ example.env.sh
@@ -1,4 +1,8 @@
 # secrets
+#export AWS_ACCESS_KEY_ID="****"
+#export AWS_SECRET_ACCESS_KEY="****"
+export AWS_REGION="eu-west-1"
+
 export INFRA_AWS_REGION="aws-region-1"
 export INFRA_AWS_S3_BUCKET="aws-s3-bucket"
 export PERSONAL_ACCESS_TOKEN="****"
