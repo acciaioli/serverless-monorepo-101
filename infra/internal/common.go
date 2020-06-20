94d2ca98d3d054b6cf3bc2c800faf3fc35487013
acciaioli <acci.valverde@protonmail.com>
2020-06-20 12:59:46 +0100

    sls deploy pls

--- infra/internal/common.go
+++ infra/internal/common.go
@@ -194,8 +194,7 @@ func (bu *BuildUtils) Deploy(env string, distZipPath string) error {
 		return err
 	}
 
-	//cmd := exec.Command("serverless", "deploy", "--stage", env)
-	cmd := exec.Command("ls", "-la")
+	cmd := exec.Command("serverless", "deploy", "--stage", env)
 	cmd.Dir = distPath
 	out := bytes.Buffer{}
 	cmd.Stdout = &out
